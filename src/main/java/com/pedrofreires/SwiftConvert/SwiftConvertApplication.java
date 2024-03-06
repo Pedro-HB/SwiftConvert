7f002cfe84c3180bd683dfefd94671d689877045
PedroH183 <pedro_freires18@outlook.com>
2024-03-05 21:47:39 -0300

    initial commit

---
+++ src/main/java/com/pedrofreires/SwiftConvert/SwiftConvertApplication.java
@@ -0,0 +1,13 @@
+package com.pedrofreires.SwiftConvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftConvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftConvertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrofreires/SwiftConvert/SwiftConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrofreires.SwiftConvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftConvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
