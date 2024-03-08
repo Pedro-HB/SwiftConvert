1d7c05939296aca8bd4a43748e141f573f02d228
PedroH183 <pedro_freires18@outlook.com>
2024-03-07 21:48:48 -0300

    fix: Change ID type

--- src/main/java/com/pedrofreires/SwiftConvert/domain/arquivos/Arquivo.java
+++ src/main/java/com/pedrofreires/SwiftConvert/domain/arquivos/Arquivo.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Document(collection = "arquivos")
 @Setter
 @Getter
 @NoArgsConstructor
@@ -14,16 +15,14 @@ import org.springframework.data.annotation.Id;
 public class Arquivo {
 
     @Id
-    public Integer id;
+    public String id;
     public String filename;
     public String pathDestino;
-    public String pathOriginal;
     public String typeDestino;
 
-    public Arquivo(String filename, String pathOriginal, String pathDestino, String ty){
+    public Arquivo(String filename, String pathDestino, String ty){
         this.typeDestino = ty;
         this.filename = filename;
         this.pathDestino = pathDestino;
-        this.pathOriginal = pathOriginal;
     }
 }
