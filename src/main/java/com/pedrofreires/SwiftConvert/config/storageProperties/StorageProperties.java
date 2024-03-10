86a39912a6d0e5b641658c4377a4608d934dee0d
PedroH183 <pedro_freires18@outlook.com>
2024-03-09 21:30:14 -0300

    fix!: Path location of StorageProperties

--- src/main/java/com/pedrofreires/SwiftConvert/config/storageProperties/StorageProperties.java
+++ src/main/java/com/pedrofreires/SwiftConvert/config/storageProperties/StorageProperties.java
@@ -7,5 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StorageProperties {
 
-    private final String location = "/DataSaved";
+    private final String location = "./DataSaved";
 }
