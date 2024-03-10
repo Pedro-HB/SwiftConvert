eff3e84d6569e4b7b0fca53ee67d0ee2ac866102
PedroH183 <pedro_freires18@outlook.com>
2024-03-09 21:32:20 -0300

    feat: implementing method to convert file to jpg

--- src/main/java/com/pedrofreires/SwiftConvert/services/ArquivoService.java
+++ src/main/java/com/pedrofreires/SwiftConvert/services/ArquivoService.java
@@ -1,42 +1,62 @@
 package com.pedrofreires.SwiftConvert.services;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
-import com.pedrofreires.SwiftConvert.domain.arquivos.Arquivo;
+import com.pedrofreires.SwiftConvert.domain.arquivo.Arquivo;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.pedrofreires.SwiftConvert.repositories.ArquivosRepository;
-import com.pedrofreires.SwiftConvert.domain.arquivos.exceptions.ArquivoNotFound;
+import com.pedrofreires.SwiftConvert.services.converter.ConverterServiceImpl;
+import com.pedrofreires.SwiftConvert.domain.arquivo.exceptions.ArquivoNotFound;
 
 
 @Service
 public class ArquivoService {
 
-    public FilesystemStorageService storageService;
-    public ArquivosRepository arquivosRepository;
+    private final ConverterServiceImpl converter;
+    private final ArquivosRepository arquivosRepository;
+    private final StorageServiceImpl filesystemStorageService;
+
 
     @Autowired
-    public ArquivoService(ArquivosRepository arquivosRepository, FilesystemStorageService storageService) {
-        this.storageService = storageService;
+    public ArquivoService(ArquivosRepository arquivosRepository, StorageServiceImpl filesystemStorageService, ConverterServiceImpl converter) {
+        this.converter = converter;
         this.arquivosRepository = arquivosRepository;
+        this.filesystemStorageService = filesystemStorageService;
     }
 
     public List<Arquivo> getAll() {
         return this.arquivosRepository.findAll();
     }
 
-    public ResponseEntity<Arquivo> getOne(Integer id) {
-        Arquivo arquivo = this.arquivosRepository.findById(id).orElseThrow(ArquivoNotFound::new); // create the arquivo not found
-        return ResponseEntity.ok().body(arquivo);
+    public Arquivo getOne(String id) {
+        return this.arquivosRepository.findById(id).orElseThrow(ArquivoNotFound::new);
     }
 
     public Arquivo create(MultipartFile file) {
 
-        String destinationPath = this.storageService.store(file);
-        Arquivo arquivo = new Arquivo(file.getOriginalFilename(), destinationPath, file.getContentType());
+        String destinationPath = this.filesystemStorageService.store(file);
+        Arquivo arquivo = new Arquivo(file.getOriginalFilename(), destinationPath, file.getContentType(), new ArrayList<>());
 
         this.arquivosRepository.save(arquivo);
         return arquivo;
+    }
+
+    public String convertPdfToJpg(String id){
+        // validar se arquivo é um pdf mesmo !!!
+        Arquivo arquivo = this.getOne(id);
+
+        if(!arquivo.mimeType.equals("application/pdf")){
+            return "O MimeType do arquivo informado não condiz com o método !";
+        }
+
+        Boolean exited_process = converter.convertPdfToJpg(arquivo);
+
+        if(!exited_process){
+            return "Não foi possível converter o arquivo !";
+        }
+        return "Conversão concluída !";
     }
 }
