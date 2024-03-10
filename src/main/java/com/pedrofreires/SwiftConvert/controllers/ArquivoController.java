b4d2a911c5f924fadbe5ace38aa28b599aad152d
PedroH183 <pedro_freires18@outlook.com>
2024-03-09 21:28:30 -0300

    feat: getViewOne in controller

--- src/main/java/com/pedrofreires/SwiftConvert/controllers/ArquivoController.java
+++ src/main/java/com/pedrofreires/SwiftConvert/controllers/ArquivoController.java
@@ -1,13 +1,13 @@
 package com.pedrofreires.SwiftConvert.controllers;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import com.pedrofreires.SwiftConvert.domain.arquivos.Arquivo;
+import com.pedrofreires.SwiftConvert.domain.arquivo.Arquivo;
 import com.pedrofreires.SwiftConvert.services.ArquivoService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/arquivo")
@@ -22,15 +22,16 @@ public class ArquivoController {
 
     @PostMapping
     public ResponseEntity<Arquivo> create(@RequestParam("file") MultipartFile file){
-        // a diferença entre o requestBody e requestParam
-        // é que o requestParam irá capturar apenas uma key do body.
-        Arquivo arquivo = this.arquivoService.create(file);
-        return ResponseEntity.ok().body(arquivo);
+        return ResponseEntity.ok().body(this.arquivoService.create(file));
     }
 
     @GetMapping
     public ResponseEntity<List<Arquivo>> getAll(){
-        List<Arquivo> arquivos = this.arquivoService.getAll();
-        return ResponseEntity.ok().body(arquivos);
+        return ResponseEntity.ok().body(this.arquivoService.getAll());
+    }
+
+    @GetMapping("/view/{id}")
+    public ResponseEntity<Arquivo> getView(@PathVariable("id") String id){
+        return ResponseEntity.ok().body(this.arquivoService.getOne(id));
     }
 }
