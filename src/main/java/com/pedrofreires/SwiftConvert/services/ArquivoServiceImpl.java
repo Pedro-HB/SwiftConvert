5ab674e56478faf537e63873b544876947e12510
PedroH183 <pedro_freires18@outlook.com>
2024-03-14 22:29:56 -0300

    feat: implementing service of get Files converted

--- src/main/java/com/pedrofreires/SwiftConvert/services/ArquivoServiceImpl.java
+++ src/main/java/com/pedrofreires/SwiftConvert/services/ArquivoServiceImpl.java
@@ -1,10 +1,14 @@
 package com.pedrofreires.SwiftConvert.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.FileSystemResource;
 import com.pedrofreires.SwiftConvert.domain.arquivo.Arquivo;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.pedrofreires.SwiftConvert.repositories.ArquivosRepository;
@@ -13,7 +17,7 @@ import com.pedrofreires.SwiftConvert.domain.arquivo.exceptions.ArquivoNotFound;
 
 
 @Service
-public class ArquivoService {
+public class ArquivoServiceImpl {
 
     private final ConverterServiceImpl converter;
     private final ArquivosRepository arquivosRepository;
@@ -21,7 +25,7 @@ public class ArquivoService {
 
 
     @Autowired
-    public ArquivoService(ArquivosRepository arquivosRepository, StorageServiceImpl filesystemStorageService, ConverterServiceImpl converter) {
+    public ArquivoServiceImpl(ArquivosRepository arquivosRepository, StorageServiceImpl filesystemStorageService, ConverterServiceImpl converter) {
         this.converter = converter;
         this.arquivosRepository = arquivosRepository;
         this.filesystemStorageService = filesystemStorageService;
@@ -58,5 +62,35 @@ public class ArquivoService {
             return "Não foi possível converter o arquivo !";
         }
         return "Conversão concluída !";
+    }
+
+    public ResponseEntity getFileConverted(String id_arquivo, String type){
+        Arquivo arquivo = this.getOne(id_arquivo);
+
+        Object[] conversoes = arquivo.getConversoes().stream().filter((arq) -> Objects.equals(arq.mimeType, type)).toArray();
+
+        if(conversoes.length < 1){
+            throw new ArquivoNotFound("Não foi possivel encontrar o arquivo !");
+        }
+        Arquivo conversao_file = (Arquivo) conversoes[0];
+
+        FileSystemResource resource = new FileSystemResource(conversao_file.location);
+        MediaType mediaType = null;
+
+        try{
+            mediaType = MediaTypeFactory.getMediaType(resource).orElseThrow(FileNotFoundException::new);
+        }catch (Exception exc){
+            exc.getStackTrace();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(mediaType);
+
+        ContentDisposition disposition = ContentDisposition.attachment()
+                .filename(resource.getFilename()).build();
+
+        headers.setContentDisposition(disposition);
+
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
     }
 }
